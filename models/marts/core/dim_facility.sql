886769bce14656c14fb6481b96fefac6c4d7559a
Saul Acevedo <saulacevedo2.0@gmail.com>
2024-02-04 13:31:19 -0600

    Update dim_facility.sql

--- models/marts/core/dim_facility.sql
+++ models/marts/core/dim_facility.sql
@@ -1,3 +1,5 @@
+-- Derived from int_header_union. The model is built by selecting only columns relating to medical facility data and a unique id is produced by hashing all columns.
+
 with unique_facility as (
     select 
     distinct {{ dbt_utils.generate_surrogate_key(['FACILITY_FEIN', 'FACILITY_NAME', 'FACILITY_ADDRESS', 'FACILITY_CITY', 'FACILITY_STATE_CODE', 'FACILITY_POSTAL_CODE', 'FACILITY_COUNTRY_CODE', 'FACILITY_CODE']) }} as FACILITY_ID,
