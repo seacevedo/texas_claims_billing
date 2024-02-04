b93c27c05843c8bc03b9874a1e0a2a8990a26333
Saul Acevedo <saulacevedo2.0@gmail.com>
2024-02-04 13:32:15 -0600

    Update dim_insurer.sql

--- models/marts/core/dim_insurer.sql
+++ models/marts/core/dim_insurer.sql
@@ -1,3 +1,5 @@
+-- Derived from int_header_union. The model is built by selecting only columns relating to insurer data and a unique id is produced by hashing all columns.
+
 with unique_insurer as (
     select 
     distinct {{ dbt_utils.generate_surrogate_key(['INSURER_FEIN', 'INSURER_POSTAL_CODE', 'DATE_INSURER_RECEIVED_BILL', 'DATE_INSURER_PAID_BILL']) }} as INSURER_ID,
