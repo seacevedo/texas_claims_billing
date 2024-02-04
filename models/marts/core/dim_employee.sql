103bf6f6c8573a26b3350c8e205e8dc2d4368018
Saul Acevedo <saulacevedo2.0@gmail.com>
2024-02-04 13:27:18 -0600

    Update dim_employee.sql

--- models/marts/core/dim_employee.sql
+++ models/marts/core/dim_employee.sql
@@ -1,3 +1,5 @@
+-- Derived from int_header_union. The model is built by selecting only columns relating to employee data and a unique id is produced by hashing all columns.
+
 with unique_employee as (
     select 
     distinct {{ dbt_utils.generate_surrogate_key(['EMPLOYEE_MAILING_CITY', 'EMPLOYEE_MAILING_STATE_CODE', 'EMPLOYEE_MAILING_POSTAL_CODE', 'EMPLOYEE_MAILING_COUNTRY', 'EMPLOYEE_DATE_OF_BIRTH', 'EMPLOYEE_GENDER_CODE', 'EMPLOYEE_MARITAL_STATUS_CODE', 'EMPLOYEE_DATE_OF_INJURY']) }} as EMPLOYEE_ID,
