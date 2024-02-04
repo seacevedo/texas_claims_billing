9ee22edd3734ab4ba76d8e303a1238d0cccb21d2
Saul Acevedo <saulacevedo2.0@gmail.com>
2024-02-04 13:32:50 -0600

    Update dim_provider.sql

--- models/marts/core/dim_provider.sql
+++ models/marts/core/dim_provider.sql
@@ -1,3 +1,5 @@
+-- Derived from int_provider_consolidate. The model is built by selecting only columns relating to provider data and a unique id is produced by hashing all columns.
+
 with unique_provider as (
     select
     {{ dbt_utils.generate_surrogate_key(['PROVIDER_FEIN', 'PROVIDER_FIRST_NAME', 'PROVIDER_LAST_NAME', 'PROVIDER_ADDRESS', 'PROVIDER_CITY', 'PROVIDER_STATE_CODE', 'PROVIDER_POSTAL_CODE', 'PROVIDER_COUNTRY', 'PROVIDER_TYPE']) }} as PROVIDER_ID,
